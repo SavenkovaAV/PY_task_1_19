bbb9ea9461611964b8de81900d0572c4010a6ae5
SavenkovaAV <90615021+SavenkovaAV@users.noreply.github.com>
2023-03-04 04:14:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-numbers_list = input("Введите числа через запятую: ")
-
-# преобразование введенной строки в список чисел
-numbers = []
-for num in numbers_list.split(','):
-    numbers.append(int(num))
+with open('input.txt', 'r') as inputFile: # Открываем файл input.txt в режиме чтения
+    lst = inputFile.readline().split()
+    newLst = []
+    for i in lst:
+        newLst.append(int(i))
 
 # создание словаря для подсчета каждого числа
 num_count = {}
-for num in numbers:
+for num in newLst:
     if num in num_count:
         num_count[num] += 1
     else:
@@ -25,4 +24,6 @@ for num, count in num_count.items():
 # сортировка списка по возрастанию
 result.sort()
 
-print(result)
+with open('output.txt', 'w') as outFile: # Открываем файл output.txt в режиме записи
+    for i in result:
+        outFile.write(str(i) + ' ')
